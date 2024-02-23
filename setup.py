39a7cfe8ca216ebe391447a7c0ea660d76e9b142
ziosta <gundoganferdi@gmail.com>
2024-02-23 21:42:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='Ziosta-Secure-Document-Registry-on-Ethereum-Blockchain',
     version='0.1',
+    url='https://github.com/ziosta/Ziosta-Secure-Document-Registry-on-Ethereum-Blockchain',
+    author='Ziosta',
+    author_email='ziosta.eth@gmail.com',
     packages=find_packages(),
     install_requires=[
-        'web3==5.24.0'  # Eğer projenizde başka bağımlılıklar varsa buraya ekleyin
+        'web3==5.24.0'
+        # Eğer projenizde başka bağımlılıklar varsa buraya ekleyin
     ],
 )
